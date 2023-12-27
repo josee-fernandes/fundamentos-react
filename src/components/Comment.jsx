e89f10613f468b425ab0a81961f611d98b29aa00
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2023-12-27 20:26:05 -0300

    Initial commit, until Iterating on JSX

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://avatars.githubusercontent.com/u/51970876?v=4"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>José Fernandes</strong>
+              <time title="11 de maio às 08:13" dateTime="2023-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} /> Aplaudir <span>03</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
